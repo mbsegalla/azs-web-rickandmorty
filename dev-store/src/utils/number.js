534656c512cd4b882626fead09fc328b11f6ae44
Marco Bruno Segalla <69018143+mbsegalla@users.noreply.github.com>
2022-01-07 11:48:15 -0300

    Delete dev-store directory

--- dev-store/src/utils/number.js
+++
@@ -1,6 +0,0 @@
-export function numberToMonetary(value) {
-  if (!value || isNaN(value)) {
-    return '';
-  }
-  return value.toLocaleString('en-US', { style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0 });
-}
